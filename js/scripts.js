1adafacdd5d2b7f8cbe5419252291e7f2ef60bf3
TAIWO SOEWU <33814162+oluwasetty@users.noreply.github.com>
2020-06-01 11:07:47 +0100

    update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $('.angle-down').click(function(){
+    $('.feature-list').css('display', 'none');
     $(this).next().toggle(3000);
       if ($(this).next().css('display') === "none") {
       $('#main-features').css('height', '2500px');
@@ -8,7 +9,7 @@ $(document).ready(function(){
   } else {
       $('#main-features').css('height', '4500px');
       $('#features').css('height', '3700px');
-      $(this).find('img').css('transform', 'rotate(180deg)')
+      $(this).find('img').css('transform', 'rotate(-180deg)')
   }
   })
 })
