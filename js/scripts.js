ec8a87d3c721f02fefbe0c147946db55ff428b8c
TAIWO SOEWU <33814162+oluwasetty@users.noreply.github.com>
2020-06-01 10:37:04 +0100

    update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
-  $('.angle-down').click(function(){
-    $(this).next().toggle(function(){
+  // $('.angle-down').click(function(){
+  //   $(this).next().toggle(function(){
+  //     alert('me')
+  //     $('#main-features').css('height', '5000px');
+  //     $('#features').css('height', '5000px');
+  //     }, function(){
+  //       alert('us')
+  //     $('#main-features').css('height', '2500px');
+  //     $('#features').css('height', '1500px');
+  //       }, 3000);
+  // })
+    $('.angle-down').toggle(function(){
+    $(this).next().css('display', 'show');
       alert('me')
       $('#main-features').css('height', '5000px');
       $('#features').css('height', '5000px');
       }, function(){
+    $(this).next().css('display', 'hide');
         alert('us')
       $('#main-features').css('height', '2500px');
       $('#features').css('height', '1500px');
         }, 3000);
-  })
 })
