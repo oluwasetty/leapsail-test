66b21d7e2e144174473fe01e01811af8998dca7e
TAIWO SOEWU <33814162+oluwasetty@users.noreply.github.com>
2020-06-01 10:26:01 +0100

    update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
   $('.angle-down').click(function(){
-    $(this).next().toggle(3000);
+    $(this).next().toggle(function(){
+      $('#main-features').css('height', '5000px');
+      $('#features').css('height', '5000px');
+      }, function(){
+      $('#main-features').css('height', '2500px');
+      $('#features').css('height', '1500px');
+        }, 3000);
+    $()
   })
 })
