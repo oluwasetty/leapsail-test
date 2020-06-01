4710e796bb3fd39eb9d419f4f5f54bfb4931c354
TAIWO SOEWU <33814162+oluwasetty@users.noreply.github.com>
2020-06-01 10:53:00 +0100

    update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,15 @@
 $(document).ready(function(){
-  // $('.angle-down').click(function(){
-  //   $(this).next().toggle(function(){
-  //     alert('me')
-  //     $('#main-features').css('height', '5000px');
-  //     $('#features').css('height', '5000px');
-  //     }, function(){
-  //       alert('us')
-  //     $('#main-features').css('height', '2500px');
-  //     $('#features').css('height', '1500px');
-  //       }, 3000);
-  // })
-    $('.angle-down').toggle(function(){
-    // $(this).next().css('display', 'show');
+  $('.angle-down').click(function(){
+    $(this).next().toggle(function(){
+      }, function(){3000);
+      if ($(this).next().css('display') === "none") {
+        alert('us')
+      $('#main-features').css('height', '2500px');
+      $('#features').css('height', '1500px');
+  } else {
       alert('me')
       $('#main-features').css('height', '5000px');
       $('#features').css('height', '5000px');
-      }, function(){
-    // $(this).next().css('display', 'hide');
-    //     alert('us')
-    //   $('#main-features').css('height', '2500px');
-    //   $('#features').css('height', '1500px');
-        });
+  }
+  })
 })
