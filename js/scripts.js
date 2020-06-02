0c9a2188d7f452ebd54d9508f28f0b4f0479dfc4
Taiwo <taiwo_soewu.1995@ymail.com>
2020-06-02 13:41:41 +0100

    added arrow rotation

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
-  $('.angle-down').click(function(){
-    $mdiv = $(this).next();
-    $('.feature-list').not($mdiv).hide()
-    $mdiv.toggle(3000);
-      if ($mdiv.css('display') === "none") {
-      $('#main-features').css('height', '2800px');
-      $('#features').css('height', '1800px');
-      $(this).find('img').css('transform', 'rotate(180deg)')
-  } else {
-      $('#main-features').css('height', '4800px');
-      $('#features').css('height', '4000px');
-      $(this).find('img').css('transform', 'rotate(-180deg)')
-  }
-  })
+$(document).ready(function() {
+    $('.angle-down img').click(function() {
+        $mdiv = $(this).parent().next();
+        $('.feature-list').not($mdiv).hide()
+        $mdiv.toggle(3000);
+        $('.angle-down img').css('transform', 'none')
+        if ($mdiv.css('display') === "none") {
+            $('#main-features').css('height', '2800px');
+            $('#features').css('height', '1800px');
+        } else {
+            $('#main-features').css('height', '4800px');
+            $('#features').css('height', '4000px');
+            $(this).css('transform', 'rotate(180deg)')
+        }
+    })
 })
