cfa1bcbea06b76e4b6e8bfc96918bcb500075e8f
Taiwo <taiwo_soewu.1995@ymail.com>
2020-06-02 14:22:02 +0100

    updated changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     $('.angle-down img').click(function() {
-        $mdiv = $(this).parent().next();
-        $('.feature-list').not($mdiv).hide()
+        $mdiv = $(this).parents('.material').next();
+        $('.feature-list').not($mdiv).not('#lg-feature-list').hide()
         $mdiv.toggle(3000);
         $('.angle-down img').css('transform', 'none')
         if ($mdiv.css('display') === "none") {
-            $('#main-features').css('height', '2800px');
-            $('#features').css('height', '1800px');
+            $('#main-features').css('height', '2500px');
+            $('#features').css('height', '1500px');
         } else {
             $('#main-features').css('height', '4800px');
             $('#features').css('height', '4000px');
